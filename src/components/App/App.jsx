785ba1b213f50235a11710895eedb281a4af41e1
Oleksandr Kostan <kostan.law@gmail.com>
2022-11-28 23:08:16 +0200

    28.11

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Section } from '../Section/Section'
+
 
 
 import { ThemeProvider } from 'styled-components';
@@ -7,7 +7,7 @@ import { theme } from './Theme';
 import { GlobalStyle } from './GlobalStyle';
 
 
-// import { nanoid } from 'nanoid'
+import { nanoid } from 'nanoid'
 // model.id = nanoid() 
 
 
@@ -17,16 +17,32 @@ export class App extends Component {
   name: ''
   };
  
+ nameId = nanoid()
 
-
+  handleInputChange = e => {
+    
+    this.setState({ name: e.currentTarget.value});
+} 
   render() {
     return (<> <ThemeProvider theme={theme}> <GlobalStyle />
-     <Section title="Phonebook">
+     <h1>Phonebook</h1>
+        <form> <label htmlFor="">
+      Name <input type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          value={this.state.name}
+          onChange={this.handleInputChange} /></label>
+      </form>
+       <button type="submit" disabled={!this.state.name}>
+          Add contact
+        </button>
         {/* Input  + кнопка*/}
-      </Section>
-        {/* <Section title="Contacts"> 
+    
+      
          {/* список контактів */}
-      {/* </Section> */} */
+    
     </ThemeProvider> </>
 
         
---
+++ src/components/ContactcForm/ContactForm.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+export class ContactForm extends Component {
+    state = {  } 
+    render() { 
+        return ();
+    }
+}
+ 
