9be7d3acea9084071460face0195ac974dca2722
Oleksandr Kostan <kostan.law@gmail.com>
2022-11-29 19:30:43 +0200

    29/11

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,26 +8,26 @@ import { GlobalStyle } from './GlobalStyle';
 
 
 import { nanoid } from 'nanoid'
-// model.id = nanoid() 
+
 
 
 export class App extends Component {
   state = {
-   contacts: [],
-  name: ''
+    contacts: [],
+    name: ''
   };
- 
- nameId = nanoid()
+
+  nameId = nanoid()
 
   handleInputChange = e => {
-    
-    this.setState({ name: e.currentTarget.value});
-} 
+
+    this.setState({ name: e.currentTarget.value });
+  }
   render() {
     return (<> <ThemeProvider theme={theme}> <GlobalStyle />
-     <h1>Phonebook</h1>
-        <form> <label htmlFor="">
-      Name <input type="text"
+      <h1>Phonebook</h1>
+      <form> <label htmlFor="">
+        Name <input type="text"
           name="name"
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
@@ -35,18 +35,18 @@ export class App extends Component {
           value={this.state.name}
           onChange={this.handleInputChange} /></label>
       </form>
-       <button type="submit" disabled={!this.state.name}>
-          Add contact
-        </button>
-        {/* Input  + кнопка*/}
-    
-      
-         {/* список контактів */}
-    
+      <button className='Button' type="submit" disabled={!this.state.name}>
+        Add contact
+      </button>
+
+
+
+      {/* список контактів */}
+
     </ThemeProvider> </>
 
-        
+
     );
   }
-} 
+}
 
