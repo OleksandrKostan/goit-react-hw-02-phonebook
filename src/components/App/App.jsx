b91dad5b38a03c832b30b36155511199c75a0663
Oleksandr Kostan <kostan.law@gmail.com>
2022-11-30 17:34:49 +0200

    300

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,14 +7,16 @@ import { nanoid } from 'nanoid';
 
 import { ContactForm } from 'components/ContactForm/ContactForm';
 import { ContactList } from 'components/ContactList/ContactList';
-
+import { Filter } from 'components/Filter/Filter';
 
 export class App extends Component {
   state = {
-    contacts: [],    
+    contacts: [{id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},], 
+    filter: ''
   };
-
-
 
  addContact = ({ name, number }) => {
     const contact = {
@@ -26,13 +28,18 @@ export class App extends Component {
       contacts: [contact, ...contacts],
     }));
    console.log(name, number)
+ };
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
   };
 
   render() {
+    const { filter } = this.state;
     return (<> <ThemeProvider theme={theme}> <GlobalStyle />
       <h2>Phonebook</h2>
       <ContactForm onSubmit={this.addContact} />
-        <h2>Contacts</h2>
+      <h2>Contacts</h2>
+      <Filter value={filter} onChange={this.addContact}/>
    <ContactList contacts={this.state.contacts}/>
 
     </ThemeProvider> </>
